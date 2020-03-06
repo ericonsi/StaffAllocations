b11665ae4135c3af4cca6d0f08d5cde850d10d2c
Eric Hirsch <ericonsi@yahoo.com>
2020-03-05 19:39:03 -0500

    x

--- Staff Allocations/My Project/Settings.Designer.vb
+++ Staff Allocations/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -29,7 +29,7 @@ Namespace My
     Private Shared addedHandlerLockObject As New Object
 
     <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
-    Private Shared Sub AutoSaveSettings(ByVal sender As Global.System.Object, ByVal e As Global.System.EventArgs)
+    Private Shared Sub AutoSaveSettings(sender As Global.System.Object, e As Global.System.EventArgs)
         If My.Application.SaveMySettingsOnExit Then
             My.Settings.Save()
         End If
@@ -68,11 +68,33 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=CHASI-FS01-TERM\SQLEXPRESS2014;Initial Catalog=Staff Allocations;Inte"& _ 
-            "grated Security=True")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=VajraB\SQLEXPRESS;Initial Catalog=STAFF ALLOCATIONS;Integrated Securi"& _ 
+            "ty=True")>  _
+        Public ReadOnly Property local_connection_string3() As String
+            Get
+                Return CType(Me("local_connection_string3"),String)
+            End Get
+        End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=Vajra-Omen\SQLEXPRESS;Initial Catalog=STAFF ALLOCATIONS;Integrated Se"& _ 
+            "curity=True")>  _
         Public ReadOnly Property local_connection_string() As String
             Get
                 Return CType(Me("local_connection_string"),String)
+            End Get
+        End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=CHASI-FS01-TERM\SQLEXPRESS2014;Initial Catalog=Staff Allocations;Inte"& _ 
+            "grated Security=True")>  _
+        Public ReadOnly Property local_connection_string12() As String
+            Get
+                Return CType(Me("local_connection_string12"),String)
             End Get
         End Property
     End Class
